9ed67115c1315f5da60a6e9f700d20f4c428b711
Vitor Leal <vitorleal1@gmail.com>
2014-08-24 21:43:48 -0300

    simple server with dianmic urls and a 404 page

--- http_server/server.go
+++ http_server/server.go
@@ -13,34 +13,28 @@ type Page struct {
 
 //Render the template
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	fmt.Printf("\n----------------------------------------\n")
-	fmt.Printf(tmpl)
-	fmt.Printf("\n----------------------------------------\n")
 	t, _ := template.ParseFiles(tmpl + ".html")
 	t.Execute(w, p)
-}
-
-//Load the page
-func loadPage(title string) (*Page, error) {
-	return &Page{Title: title}, nil
 }
 
 //Http handler
 func handler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[1:]
-	p, err := loadPage(title)
+	page := &Page{Title: title}
 
-	if err != nil {
-		http.Redirect(w, r, "404", http.StatusFound)
-		return
-	}
+	renderTemplate(w, "view/index", page)
+}
 
-	renderTemplate(w, "view/" + title, p)
+//Not found page
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	page := &Page{Title: "Not found"}
+	renderTemplate(w, "view/404", page)
 }
 
 //Main
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/404", notFoundHandler)
 
 	fmt.Printf("Listening in the port :4000")
 	http.ListenAndServe(":4000", nil)
